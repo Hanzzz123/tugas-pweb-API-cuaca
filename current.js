a87dd18d5efe1fb60622c333d3688b491955ff18
Hanzzz123 <aanzs496@gmail.com>
2023-10-05 23:49:47 +0700

    callback function, API request, Json

--- current.js
+++ current.js
@@ -1,40 +1,42 @@
+//callback function step 1 button click
 $(document).ready(function(){
-
-    $("#submitCity").click(function(){
-        return getWeather();
+    $("#form-submit").submit(function(event){
+        performSearch(event);
     });
+});
 
+//api request
+function performSearch(event){
+    var request;
+    event.preventDefault();
+
+    //asynchronous call
+
+   request = $.ajax({
+        url:'https://api.openweathermap.org/data/2.5/weather',
+        type: "GET",
+        data : {
+            q: $("#inputCity").val(),
+            appid: '88762884948b847a1c3ba479c6fa56d4',
+            units: 'metric'
+        }
 
 });
-function getWeather(){
-    var city = $("#city").val();
 
-    if(city != ''){
+   //step 3
+   request.done(function (response){
+       formatSearch(response);
+    });
 
-        $.ajax({
-            url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=metric" + "&APPID=c10bb3bd22f90d636baa008b1529ee25",
-            type: "GET",
-            dataType: "jsonp",
-            success: function(data){
-                var widget = showResults(data)
-
-
-                $("#showWeather").html(widget);
-
-                $("#city").val('');
-            }
-
-        });
-
-
-    }else{
-        $("#error").html("<div class='alert alert-danger' id='errorCity'><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>City field cannot be empty</div>");
-    }
 }
 
-function showResults(data){
-    return  '<h2 style="font-weight:bold; font-size:30px; padding-top:20px;" class="text-center">Current Weather for '+data.name+', '+data.sys.country+'</h2>'+
-        "<h3 style='padding-left:40px;'><strong>Weather</strong>: "+data.weather[0].main+"</h3>"+
-        "<h3 style='padding-left:40px;'><strong>Description</strong>:<img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'> "+data.weather[0].description+"</h3>"+
-        "<h3 style='padding-left:40px;'><strong>Temperature</strong>: "+data.main.temp+" &deg;C</h3>"
+function formatSearch(jsonObject){
+    var city_name = jsonObject.name;
+    var city_weather = jsonObject.weather[0].main;
+    var city_temp = jsonObject.main.temp
+
+    $("#city-name").text(city_name);
+    $("#city-weather").text(city_weather);
+    $("#city-temp").text(city_temp + " Celsius");
+
 }
